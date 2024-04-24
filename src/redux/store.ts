b95be9df499067ce66607730aa6f3e704f07b986
kiran-2608 <141059641+kiran-2608@users.noreply.github.com>
2024-04-24 14:46:58 +0530

    Update store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,39 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import { productAPI } from "./api/productAPI";
+// import { userAPI } from "./api/userAPI";
+
+// import { cartReducer } from "./reducer/cartReducer";
+// import { orderApi } from "./api/orderAPI";
+// import { userReducer } from "./reducer/userReduer";
+// import { dashboardApi } from "./api/dashboardAPI";
+
+// export const server = import.meta.env.VITE_SERVER;
+
+// export const store = configureStore({
+//   reducer: {
+//     [userAPI.reducerPath]: userAPI.reducer,
+//     [productAPI.reducerPath]: productAPI.reducer,
+//     [orderApi.reducerPath]: orderApi.reducer,
+//     [dashboardApi.reducerPath]: dashboardApi.reducer,
+//     [userReducer.name]: userReducer.reducer,
+//     [cartReducer.name]: cartReducer.reducer,
+//   },
+//   middleware: (mid) => [
+//     ...mid(),
+//     userAPI.middleware,
+//     productAPI.middleware,
+//     orderApi.middleware,
+//     dashboardApi.middleware,
+//   ],
+// });
+
+// export type RootState = ReturnType<typeof store.getState>;
+
+
+
 import { configureStore } from "@reduxjs/toolkit";
 import { productAPI } from "./api/productAPI";
 import { userAPI } from "./api/userAPI";
-
 import { cartReducer } from "./reducer/cartReducer";
 import { orderApi } from "./api/orderAPI";
 import { userReducer } from "./reducer/userReduer";
@@ -18,13 +50,18 @@ export const store = configureStore({
     [userReducer.name]: userReducer.reducer,
     [cartReducer.name]: cartReducer.reducer,
   },
-  middleware: (mid) => [
-    ...mid(),
-    userAPI.middleware,
-    productAPI.middleware,
-    orderApi.middleware,
-    dashboardApi.middleware,
-  ],
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware({
+      thunk: true, // Ensure thunk middleware is enabled
+      immutableCheck: false, // Disable immutable state check for performance
+      serializableCheck: false, // Disable serializable state check for performance
+    }).concat([
+      userAPI.middleware,
+      productAPI.middleware,
+      orderApi.middleware,
+      dashboardApi.middleware,
+    ]);
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
